6ca2d3a1bdc96a941c326b6cc0f8d11778f1ff13
aditystomar-ct <aditya.tomar@clear.in>
2025-01-11 14:28:39 +0530

    just understanding about const var and let

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,24 @@
+const accountId=14532;
+let accountEmail = "hitesh@google.com";
+var accountPassword = "12345";
+accountCity = "jaipur"; //not recommeded to use
+let accountState;
+
+//accountId = 2; not allowed because of consrt no reintialization and need to be intialized at the time of declaration
+
+accountEMail = "aditya@gmail.com";
+accountPassword = "4321";
+accountCity = "Allahabad"
+
+//var is not recommeded because of scope issue
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState]);
+
+
+
+
