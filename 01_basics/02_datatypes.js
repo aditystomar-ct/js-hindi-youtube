52074d4925bfb85d6d0b0f536f5e108b36658f7f
aditystomar-ct <aditya.tomar@clear.in>
2025-01-11 14:43:55 +0530

    understand about some datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) //we are using nodejs not browser that is why
+
+
+let name = "Aditya";
+let age = 22;
+let isLoggedIn = true;
+let state;
+
+//number => 2 to the power 53
+//bigint
+//string =>""
+//boolean
+//null =>standalone value
+//symbol =>uniqueness
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
